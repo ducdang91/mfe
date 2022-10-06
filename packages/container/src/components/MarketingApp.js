86e443140701962762b2ace2d4be68e4b170d977
Duc Dang <dtduc1991@gmail.com>
2022-10-06 17:04:12 +0700

    Update Marketing

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -2,10 +2,11 @@ import { mount } from 'marketing/MarketingApp';
 import React, { useRef, useEffect } from 'react';
 
 export default () => {
-    const ref = useRef(null);
+  const ref = useRef(null);
 
-    useEffect(() => {
-        mount(ref.current);
-    })
-    return <div ref={ref} />
-}
+  useEffect(() => {
+    mount(ref.current);
+  });
+
+  return <div ref={ref} />;
+};
