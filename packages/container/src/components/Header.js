d4e3ad3cc9fef92d2f56619b158fbd91740261ef
Duc Dang <dtduc1991@gmail.com>
2022-10-06 16:53:48 +0700

    Update Dev Branch

--- packages/container/src/components/Header.js
+++ packages/container/src/components/Header.js
@@ -79,7 +79,7 @@ export default function Header({ signedIn, onSignOut }) {
             component={RouterLink}
             to="/"
           >
-            App
+            App2
           </Typography>
           <Button
             color="primary"
