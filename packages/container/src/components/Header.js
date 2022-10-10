40ae32a932573755f395cf32351e9dea81d720a2
Duc Dang <dtduc1991@gmail.com>
2022-10-10 14:52:15 +0700

    Auth Component

--- packages/container/src/components/Header.js
+++ packages/container/src/components/Header.js
@@ -79,7 +79,7 @@ export default function Header({ signedIn, onSignOut }) {
             component={RouterLink}
             to="/"
           >
-            App1
+            App
           </Typography>
           <Button
             color="primary"
